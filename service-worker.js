3e2a555827241d6a293d645089f1b44a95349620
Pete Lippincott <petesimple@gmail.com>
2026-02-13 14:25:07 -0600

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "classic-skeeball-v0.1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./main.js",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
